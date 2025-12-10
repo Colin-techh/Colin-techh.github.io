05a387123922133865a4a4ea92a40f50676aa478
ColinW <colinnw15@gmail.com>
2025-12-10 15:57:37 -0500

    m

---
+++ src/components/atoms/GetSource.jsx
@@ -0,0 +1,11 @@
+function GetSource({source}) {
+    return(
+        <div>
+            <button
+                className="modern-button"
+                onClick={source}
+                >See Source of Text</button>
+        </div>
+    );
+}
+export default GetSource;
